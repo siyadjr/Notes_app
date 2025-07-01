c54b40cee1b2d36b9b3da2447d6bd6975a94854b
siyadjr <siyadsiyad016@gmail.com>
2025-07-01 16:37:30 +0530

    Initial commit: Flutter Notes App with Firebase setup

---
+++ lib/presentation/auth/pages/sign_in_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+return Scaffold(
+  body: Center(child: Text('sign in'),),
+);  }
+}
